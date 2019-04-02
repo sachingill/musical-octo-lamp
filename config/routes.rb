fdc00fd51368343809a83073cb91ddec1d98dd96
sachingill <sachin.gill07@gmail.com>
2019-04-03 02:12:56 +0530

    Commenting devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.htm
   get 'home/index', to: 'home#index' 
   root to: 'home#index'
